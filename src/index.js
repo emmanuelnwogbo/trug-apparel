612e2f39cea7845c1f5cee537822e07bbc46d320
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2019-07-26 00:51:52 +0100

    first build before first deploy

--- src/index.js
+++ src/index.js
@@ -13,6 +13,8 @@ app.get('/', (req, res) => {
   res.send(renderer());
 });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
   console.log('Listening on port 3000');
 });
