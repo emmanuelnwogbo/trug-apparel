1752d04c5763b1882592feccd47b26ebf707ad2e
Emmanuel Nwogbo <nerdyemmanuel@gmail.com>
2019-08-16 23:25:03 +0100

    set-up contentful

--- src/client/client.js
+++ src/client/client.js
@@ -2,6 +2,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import * as contentful from 'contentful';
+
+const client = contentful.createClient({
+  space: 'deblftxe5zjg',
+  accessToken: '2WG9t8vIqQKelVBrXii2gXvoUZwFGqwo0oEWM2pv8ek' })
+client.getEntries().then(entries => {
+  entries.items.forEach(entry => {
+    if(entry.fields) {
+      console.log(entry.fields.category)
+    }
+  })
+})
 
 import Home from './components/Home';
 import './scss/main.scss'
